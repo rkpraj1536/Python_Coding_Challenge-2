7a1eeb21c5757cd070f986de725011675c70161a
Rahul Prajapati <112754941+rkpraj1536@users.noreply.github.com>
2026-04-09 10:09:20 +0530

    done

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+# factorial calculator
+
+n = int(input("Enter number: "))
+
+fact = 1
+
+for i in range(1, n+1):
+    fact = fact * i
+
+print(fact)
---
+++ Palindrom_Checker.py
@@ -0,0 +1,15 @@
+# palindrome number
+
+num = int(input("Enter number: "))
+temp = num
+rev = 0
+
+while num > 0:
+    d = num % 10
+    rev = rev * 10 + d
+    num = num // 10
+
+if temp == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
