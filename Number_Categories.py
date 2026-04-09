e1f451b385cb30efea88d7424c607865b2d0d4d9
Rahul Prajapati <112754941+rkpraj1536@users.noreply.github.com>
2026-04-09 09:58:36 +0530

    number categories calculator

---
+++ Number_Categories.py
@@ -0,0 +1,10 @@
+# positive negative zero categories
+
+n = int(input("Enter number: ")) # taking input from user 
+
+if n > 0:
+    print("Positive")
+elif n < 0:
+    print("Negative")
+else:
+    print("Zero")
