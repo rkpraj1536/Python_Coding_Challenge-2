8179de8082708a79917aa9ac039b4397aba204d5
Rahul Prajapati <112754941+rkpraj1536@users.noreply.github.com>
2026-04-09 10:52:10 +0530

    done

---
+++ ListToTupe.py
@@ -0,0 +1,7 @@
+# list to tuple converter
+
+lst = [1,2,3]
+
+t = tuple(lst) #expicity converting into tuple and store it into t variabe
+
+print(t)
---
+++ Sorting.py
@@ -0,0 +1,10 @@
+# manual sorting
+
+lst = [3,1,2]
+
+for i in range(len(lst)):
+    for j in range(i+1,len(lst)):
+        if lst[i] > lst[j]:
+            lst[i],lst[j] = lst[j],lst[i]
+
+print(lst)
